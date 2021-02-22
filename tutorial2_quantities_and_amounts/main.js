787582824f6dce60a095e4dda597ccde17d25972
valderete <valderete@gradcenter.cuny.edu>
2021-02-22 02:02:30 -0600

    updates to Tutorial 2

--- tutorial2_quantities_and_amounts/main.js
+++ tutorial2_quantities_and_amounts/main.js
@@ -2,29 +2,59 @@ d3.csv('../data/squirrelActivities.csv', d3.autoType)
 .then(data => {
     console.log("data", data)
 
-const width = window.innerWidth * .8;
-const height = 500;
+// constants
+const width = window.innerWidth *.8 ;
+const height = window.innerHeight/2;
+const margin = ({ top: 20, right: 20, bottom: 0, left: 60 })
+const color = d3.scaleSequential()
+    .domain([10, d3.max(data, d => d.count)])
+    .interpolator(d3.scaleOrdinal(d3.schemeAccent))
 
-const xScale = d3.scaleBand()
-  .domain(data.map(d=> d.activity))
-  .range([0, width]) // visual variable
-  .paddingInner(.2)
+// SCALES
+// xscale - linear,count
+const xScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d=> d.count)])
+    .range([height, 50])
 
-const yScale = d3.scaleLinear()
-  .domain([0, d3.max(data, d=> d.count)])
-  .range([height, 0])
+  // yscale - categorical, activity
+const yScale = d3.scaleBand()
+    .domain(data.map(d=> d.activity))
+    .range([450, margin.top, height - margin.bottom])
+    .paddingInner(.1)
 
+// svg
 const svg = d3.select("#barchart")
   .append("svg")
-  .attr("width", width)
-  .attr("height", height)
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
 
+// bars
 svg.selectAll("rect")
   .data(data)
   .join("rect")
-  .attr("width", xScale.bandwidth())
-  .attr("height", d=> height - yScale(d.count))
-  .attr("x", d=>xScale(d.activity))
-  .attr("y", d=> yScale(d.count))
+  .attr("height", yScale.bandwidth())
+  .attr("width", d=> width - - margin.left - xScale(d.count))
+  .attr("transform", `translate(100, ${height - margin.middle})`)
+  .attr("y", d=>yScale(d.activity))
+  .attr("x", 60, d=> xScale(d.count))
+  .attr("fill", d=>color(d.count))
 
+  //activity left
+svg.selectAll("text.activity")
+    .data(data)
+    .join("text")
+    .attr("class",'activity')
+    .attr("y",d=> yScale(d.activity)+(yScale.bandwidth()/2))
+    .attr("x",0,d=> xScale(d.count))
+    .attr("text-anchor",'left')
+    .text(d=> d.activity)
+
+//count right
+svg.selectAll("text.count")
+    .data(data)
+    .join("text")
+    .attr("class",'count')
+    .attr("y",d => yScale(d.activity)+(yScale.bandwidth()/2))
+    .attr("x",d=> 70)
+    .text(d=> d.count)
 })
